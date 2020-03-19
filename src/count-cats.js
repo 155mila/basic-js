0ea027574e1716d614ac68a04aa38ae2a7aff537
155mila <58401380+155mila@users.noreply.github.com>
2020-03-19 17:07:36 +0300

    feat: refresh count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 module.exports = function countCats(backyard) {
   let counter = 0;
   for(let j = 0; j < backyard.length; j++){
-    for(let i = 0; i[j] < backyard.length; i++){
+    for(let i = 0; i < backyard[j].length; i++){
       if(backyard[j][i] === "^^")
       counter++;
     }
