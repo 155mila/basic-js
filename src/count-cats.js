2c120634912f16e04db89bfddd0e9c3b3c429dd9
155mila <58401380+155mila@users.noreply.github.com>
2020-03-19 17:17:36 +0300

    refresh count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,8 @@
-module.exports = function countCats(backyard) {
+module.exports = function countCats(matrix) {
   let counter = 0;
-  for(let j = 0; j < backyard.length; j++){
-    for(let i = 0; i < backyard[j].length; i++){
-      if(backyard[j][i] === "^^")
+  for(let j = 0; j < matrix.length; j++){
+    for(let i = 0; i < matrix[j].length; i++){
+      if(matrix[j][i] === "^^")
       counter++;
     }
     return counter;
