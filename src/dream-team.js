9a298e89ebd9e086b6f9464bd3192b36bba6ef81
155mila <58401380+155mila@users.noreply.github.com>
2020-10-06 13:40:01 +0300

    feat: add resolve dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)){
+    return false
+  }
+  let result = ''
+  members.forEach(item =>{
+    if(typeof(item) === 'string'){
+      result += item.split('')[0].toUpperCase()
+    }
+    })
+    return result.split('').sort().join('')
 };
