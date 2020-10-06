fbbed0ccc8ff57a5501af42476efc478e6b7e036
155mila <58401380+155mila@users.noreply.github.com>
2020-10-06 13:54:05 +0300

    feat: refresh carbon-dating count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,7 @@ const HALF_LIFE_PERIOD= 5730;
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity == 'string' && parseFloat(sampleActivity) < 0){
     let k = 0.693/HALF_LIFE_PERIOD;
-    let res = Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity));
+    let res = Math.log(parseFloat(sampleActivity)/MODERN_ACTIVITY);
     let t = Math.ceil(res/k);
     return t; 
   }else{
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,10 +1,10 @@
-module.exports = function countCats(matrix) {
-  let counter = 0;
-  for(let j = 0; j < matrix.length; j++){
-    for(let i = 0; i < matrix[j].length; i++){
-      if(matrix[j][i] === "^^")
-      counter++;
+module.exports = function countCats(Array) {
+  let number = 0;
+  for(let j = 0; j < Array.length; j++){
+    for(let i = 0; i < Array[j].length; i++){
+      if(Array[j][i] === "^^")
+      number++;
     }
-    return counter;
+    return number;
   }
 };
